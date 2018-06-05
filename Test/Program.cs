a59fbe41a20681fdf71ea1db7e43c0e0720979d9
shivanish <shivanish@SHIVANISH-W10>
2018-06-05 11:35:58 +0530

    Revert "Added surname also"
    This reverts commit 92f8beb1e2dec0edb0ea32fa62654106f40a292a.

--- Test/Program.cs
+++ Test/Program.cs
@@ -10,7 +10,7 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World Shivani Shah!!");
+            Console.WriteLine("Hello World Shivani!!");
         }
     }
 }
