ab840f88b3af747cb0294ddea380fb97cb348b99
shivanish <shivanish@SHIVANISH-W10>
2018-06-05 12:17:29 +0530

    Direct merge without pull request

--- Test/Program.cs
+++ Test/Program.cs
@@ -10,7 +10,7 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World Shivani Shah in Development!!");
+            Console.WriteLine("Hello World Shivani Shah in Development Direct merge!!");
         }
     }
 }
